a82d02c3b7b6e36a79298b986c824f49598668ae
Виктор Брылёв <msne1488@gmail.com>
2023-02-28 22:05:52 +0300

    todo

--- index.js
+++ index.js
@@ -1,59 +1,46 @@
-function squareCalc (x1, y1, x2, y2) {
-
-    let width = Math.abs(x1 - x2);
-    let height = Math.abs(y1 - y2);
-
-    let square = width * height
-
-    console.log('Площадь прямоугольника равна', square)
-
-}
-
-function fractionalPart (a, b, n) {
-
-    let fractA = Math.floor((a % 1) * Math.pow(10, n));
-    let fractB = Math.floor((b % 1) * Math.pow(10, n)); 
-
-    console.log('Сравнение чисел после запятой:')
-    console.log(fractA > fractB);
-    console.log(fractA < fractB);
-    console.log(fractA >= fractB);
-    console.log(fractA <= fractB);
-    console.log(fractA === fractB);
-    console.log(fractA != fractB);
-
-}
-
-function rand (n, m) {
-    
-    let range = m - n;
-    if (n > m) {
-        range = n - m
+(function() {
+    function createAppTitle (title) {
+        let appTitle = document.createElement('h2');
+        appTitle.innerHTML = title;
+        return appTitle;
     }
-    
-    let first = Math.round(Math.random() * range);
-    let second = Math.round(Math.random() * range);
-    let min = Math.min(m, n);
-    
-    let a = min + first;
-    let b = min + second;
 
-    console.log('сравнение рандомных чисел:')
-    console.log(a, b);
-    console.log(a > b);
-    console.log(a < b);
-    console.log(a >= b);
-    console.log(a <= b);
-    console.log(a === b);
-    console.log(a != b);
-    
-}
+    function createTodoItemForm () {
 
-//Площадь
-squareCalc(8, 1, 5, 1);
+        let form = document.createElement('form');
+        let input = document.createElement('input');
+        let buttonWrapper = document.createElement('div');
+        let button = document.createElement('button');
 
-//Сравнение чисел после запятой
-fractionalPart(13.123456789, 2.123, 5);
+        form.classList.add('input-group', 'mb-3');
+        input.classList.add('form-control');
+        input.placeholder = 'Добавьте новую задачу';
+        buttonWrapper.classList.add('input-group-append');
+        button.classList.add('btn', 'btn-primary');
+        button.textContent = 'Добавить задачу';
+        
+        buttonWrapper.append(button);
+        form.append(input);
+        form.append(buttonWrapper);
 
-//Сравнение рандомных чисел
-rand(-3, -10)
+        return {form, input, button};
+    }
+
+    function createTodoList () {
+        let list = document.createElement('ul');
+        list.classList.add('group-list');
+        return list;
+    }
+
+    document.addEventListener('DOMContentLoaded', function() {
+        let container = document.getElementById('todo-app');
+
+        let todoAppTitle = createAppTitle('Список задач');
+        let todoAppForm = createTodoItemForm();
+        let todoAppList = createTodoList();
+
+        container.append(todoAppTitle);
+        container.append(todoAppForm.form);
+        container.append(todoAppList);
+    });
+})();
